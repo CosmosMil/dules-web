5d6a10629922c882db5549c88e239151143ec1ea
CosmosMil <107584409+CosmosMil@users.noreply.github.com>
2023-08-20 17:07:32 +0200

    styling and revision of /about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -18,13 +18,13 @@ function About(props: Props) {
             height={400}
             width={500} /></div>
 
-          <div className='p-8 text-justify lg:text-lg'>
-            <div> Dušan Solomun is a Berlin-based director, cinematographer and photographer. He graduated from FH Dortmund with a major in film and television camera and documentary filmmaking. During his studies at the FH Dortmund Solomun spent one year at the Bezalel Arts Academy in Jerusalem, where he realized his first documentary <Link className='text-purple-600' href='/lod-on-the-map'>LOD ON THE MAP!</Link> about two musicians from a small town in the heart of Israel, dreaming about a career. The film premiered at the Israel Film Festival Berlin in 2012.</div>
+          <div className='p-8 mr-8 text-justify lg:text-lg'>
+            <div> Dušan Solomun is a Berlin-based director, cinematographer and photographer. He graduated from FH Dortmund with a major in film and television camera and documentary filmmaking. During his studies at the FH Dortmund, Solomun spent a year at the Bezalel Arts Academy in Jerusalem. There, he produced his debut full length documentary <Link className='text-purple-600' href='/lod-on-the-map'>LOD ON THE MAP!</Link> about two musicians from a small town in the heart of Israel, dreaming of a career. The film premiered at the Israel Film Festival Berlin in 2012.</div>
 
-            <div className='mt-5'> Back in Berlin Solomun developed several film and theater workshops for refugees together with Syrian director Rafat Alzakout.
-              In 2019 Solomun finalized his docufiction <Link className='text-purple-600' href='/children-of-spring'>CHILDREN OF SPRING</Link>, with refugees reenacting the most dangerous situation of their journey to Europe: The crossing of the Mediterranean Sea. Children of Spring screened at various international film festivals including Beldocs, FilmFestival Cottbus, Buffalo IFF and won the mid-length feature film main prize at Salto Independent Film Festival and at the Conception Independent Film Awards.</div>
+            <div className='mt-5'> Upon returning to Berlin, Solomun collaborated with Syrian director Rafat Alzakout to create several film and theater workshops for refugees.
+              In 2019 Solomun completed his docufiction <Link className='text-purple-600' href='/children-of-spring'>CHILDREN OF SPRING</Link>. The movie features refugees reenacting the most dangerous part of their journey to Europe: The crossing of the Mediterranean Sea. Children of Spring screened at various international film festivals including Beldocs, FilmFestival Cottbus and Buffalo IFF. It won the mid-length feature film main prize at both the Salto Independent Film Festival and the Conception Independent Film Awards.</div>
 
-            <div className='mt-5'>Solomuns first feature film <Link className='text-purple-600' href='number-my-wanderings'>NUMBER MY WANDERINGS</Link> (in Collaboration with Cinematographer <a href='https://www.tilohauke.de/' >TILO HAUKE</a>) premiered in spring 2023 at the Achtung Berlin Film Festival.</div>
+            <div className='mt-5'>Solomun&apos;s first feature film <Link className='text-purple-600' href='number-my-wanderings'>NUMBER MY WANDERINGS</Link>,in collaboration with Cinematographer <a href='https://www.tilohauke.de/' >TILO HAUKE</a>, premiered in spring 2023 at the <a href='https://achtungberlin.de/zaehle-die-wege-meiner-flucht/'>ACHTUNG BERLIN FILM FESTIVAL.</a></div>
 
           </div>
 
