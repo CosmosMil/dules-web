f0cb5a08a67b686291ea607fc4de7667b2ee1920
CosmosMil <107584409+CosmosMil@users.noreply.github.com>
2023-08-25 16:46:54 +0200

    new array, smaller sized pics

--- src/app/photo-gallery/page.tsx
+++ src/app/photo-gallery/page.tsx
@@ -9,15 +9,30 @@ import Modal from '../../components/Modal'
 
 type Props = {}
 
-const animalPics = [
-  { name: 'nap', url: 'https://res.cloudinary.com/dte85mpkw/image/upload/v1692612088/dule/gallery/nap.jpg', height: 350, width: 500 },
-  { name: 'sunday stroll', url: 'https://res.cloudinary.com/dte85mpkw/image/upload/v1692611838/dule/gallery/sunday-stroll.jpg', height: 450, width: 350 },
+const pics = [
+  { name: 'seek and hide', url: 'https://res.cloudinary.com/dte85mpkw/image/upload/v1692971132/dule/gallery/seek-and-hide.jpg', height: 350, width: 500 },
 
-  { name: 'beach life', url: 'https://res.cloudinary.com/dte85mpkw/image/upload/v1692612273/dule/gallery/beach-dogs.jpg', height: 350, width: 500 },
+  { name: 'eggs and phone', url: 'https://res.cloudinary.com/dte85mpkw/image/upload/v1692972574/dule/gallery/eggs_and_phone.jpg', height: 450, width: 350 },
+
+  { name: 'a taste of berlin', url: 'https://res.cloudinary.com/dte85mpkw/image/upload/v1692972370/dule/gallery/a_taste_of_berlin.jpg', height: 450, width: 350 },
+
+  { name: 'total damage', url: 'https://res.cloudinary.com/dte85mpkw/image/upload/v1692972894/dule/gallery/total_damage.jpg', height: 450, width: 350 },
+
+  { name: 'take a pic', url: 'https://res.cloudinary.com/dte85mpkw/image/upload/v1692973209/dule/gallery/take_a_pic.jpg', height: 350, width: 500 },
+
+  { name: 'sunday stroll', url: 'https://res.cloudinary.com/dte85mpkw/image/upload/v1692973543/dule/gallery/sunday_stroll.jpg', height: 450, width: 350 },
+
+  { name: 'fragments', url: 'https://res.cloudinary.com/dte85mpkw/image/upload/v1692973898/dule/gallery/fragments.jpg', height: 450, width: 350 },
+
+  { name: 'nap', url: 'https://res.cloudinary.com/dte85mpkw/image/upload/v1692974177/dule/gallery/nap.jpg', height: 350, width: 500 },
+
+  { name: 'jump to the future', url: 'https://res.cloudinary.com/dte85mpkw/image/upload/v1692612176/dule/gallery/jump-to-the-future.jpg', height: 350, width: 500 },
+
   { name: 'the sky is theirs', url: 'https://res.cloudinary.com/dte85mpkw/image/upload/v1692612328/dule/gallery/the-sky-is-theirs.jpg', height: 350, width: 500 },
-  { name: 'whats next', url: 'https://res.cloudinary.com/dte85mpkw/image/upload/v1692611927/dule/gallery/whats-next.jpg', height: 450, width: 350 },
 
-  { name: 'bird watch', url: 'https://res.cloudinary.com/dte85mpkw/image/upload/v1692611579/dule/gallery/bird%20watch.jpg', height: 450, width: 350 },
+  { name: 'fragments2', url: 'https://res.cloudinary.com/dte85mpkw/image/upload/v1692974394/dule/gallery/fragments2.jpg', height: 450, width: 350 },
+
+  { name: 'bird watch', url: 'https://res.cloudinary.com/dte85mpkw/image/upload/v1692974549/dule/gallery/bird-watch.jpg', height: 450, width: 350 },
 
 ]
 
@@ -31,17 +46,17 @@ function PhotoGallery(props: Props) {
       {modalOpen && <Modal src={selectedImage} closeModal={() => setModalOpen(false)} />}
       <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
         <Masonry>
-          {animalPics.map((animal) => (
-            <div key={animal.name} className='flex justify-center p-2'>
+          {pics.map((pic) => (
+            <div key={pic.name} className='flex justify-center p-2'>
 
               <Image
-                height={animal.height}
-                width={animal.width}
-                src={animal.url}
+                height={pic.height}
+                width={pic.width}
+                src={pic.url}
                 alt=''
                 style={{ cursor: 'pointer' }}
                 onClick={() => {
-                  setSelectedImage(animal.url);
+                  setSelectedImage(pic.url);
                   setModalOpen(true)
                 }}
               />
