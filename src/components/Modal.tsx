dfcf3be91a382465baa2f999416c542499e2f5fb
CosmosMil <107584409+CosmosMil@users.noreply.github.com>
2023-10-02 15:54:09 +0200

    modal to close also on bg click

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from 'next/image'
-import Layout from '../../src/app/layout'
+
 
 type Props = {
   src: string | null;
@@ -13,19 +13,18 @@ const Modal: React.FC<Props> = ({ src, closeModal }) => {
 
   return (
 
-    <div className='fixed top-0 left-0 w-full h-full bg-black bg-opacity-75 flex justify-center items-center'>
+    <div className='fixed top-0 left-0 w-full h-full bg-black bg-opacity-80 flex justify-center items-center' onClick={closeModal}>
       {/* <div className="p-4 font-bold h-4/5 w-3/4 bg-slate-700"> */}
 
       {/* <div className="relative w-full h-full"> */}
 
       {/* <button onClick={closeModal} className="absolute top-5 right-5 z-10 flex items-center justify-center bg-white h-8 w-8 rounded-full p-2">X</button> */}
-      <Image
+      <img
         src={src}
-        layout='fill'
-        objectFit='contain'
         alt=''
+
         onClick={closeModal}
-        style={{ cursor: 'pointer' }}
+        style={{ cursor: 'pointer', maxWidth: '100%', maxHeight: '85vh' }}
       />
 
 
@@ -39,4 +38,4 @@ const Modal: React.FC<Props> = ({ src, closeModal }) => {
   )
 }
 
-export default Modal
+export default React.memo(Modal);
