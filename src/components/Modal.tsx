6723cd5c7bdf91bb856f66974f35dfcaec9a58e1
CosmosMil <107584409+CosmosMil@users.noreply.github.com>
2023-11-01 17:16:20 +0100

    clean up

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -14,11 +14,6 @@ const Modal: React.FC<Props> = ({ src, closeModal }) => {
   return (
 
     <div className='fixed top-0 left-0 w-full h-full bg-black bg-opacity-80 flex justify-center items-center' onClick={closeModal}>
-      {/* <div className="p-4 font-bold h-4/5 w-3/4 bg-slate-700"> */}
-
-      {/* <div className="relative w-full h-full"> */}
-
-      {/* <button onClick={closeModal} className="absolute top-5 right-5 z-10 flex items-center justify-center bg-white h-8 w-8 rounded-full p-2">X</button> */}
       <img
         src={src}
         alt=''
